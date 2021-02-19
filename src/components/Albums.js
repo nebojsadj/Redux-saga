8421f7cd3e9ec55640ebc74c49e8710faf1ae193
Nebojsadj <djordjevicn87@gmail.com>
2021-02-19 14:17:09 +0100

    adding component

--- src/components/Albums.js
+++ src/components/Albums.js
@@ -1,25 +1,22 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import Photo from "./Photo";
 
 function Albums() {
   const albums = useSelector((state) => state.albums.albums);
+  const albumId1 = albums.filter((el) => el.albumId === 1);
+  console.log(albumId1);
   return (
     <div className="container-fluid">
       <div className="row">
         <div className="col-10 offset-1">
           <h1 className="text-center">Albums</h1>
-          <div className="row">
-            <div className="col-6 offset-3">
-              {albums.map((album) => (
-                <img
-                  src={album.thumbnailUrl}
-                  alt={album.id}
-                  key={album.id}
-                ></img>
-              ))}
-            </div>
-          </div>
         </div>
+      </div>
+      <div className="row">
+        {albumId1.map((album) => (
+          <Photo album={album} key={album.id} />
+        ))}
       </div>
     </div>
   );
